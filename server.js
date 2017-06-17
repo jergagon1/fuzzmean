bc54cda88a99026cf00ec620d1582e308d2b174f
Jeremy Gagon <jergagon1@gmail.com>
2017-06-17 09:45:53 -0700

    setup stuff

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('blah');
+// console.log('blah');
+const express = require('express');
+const app = express();
+
+app.listen(3000, function(){
+  console.log('listening on 3000')
+})
+
+app.get('/', (req,res) =>{
+  res.sendFile(__dirname + '/index.html')
+})
